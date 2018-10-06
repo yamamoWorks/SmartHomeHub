423248e1e94fef88b70bb957560b4b7f618baf00
yamamo <mail.to.yamamo@gmail.com>
2018-10-06 18:17:22 +0900

    implement cec

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const Mqtt = require("azure-iot-device-mqtt").Mqtt;
 const Client = require("azure-iot-device").Client;
 const Device = require("ps4-waker").Device;
 require('log-timestamp');
+const { exec } = require("child_process");
 
 const connectionString = process.env.IOTHUB_CONNECTION_STRING;
 const client = Bluebird.promisifyAll(Client.fromConnectionString(connectionString, Mqtt));
@@ -31,9 +32,24 @@ const onSendPS4Command = (request, response) => {
 		});
 };
 
+const onSendCecCommand = (request, response) => {
+	console.log(request.payload);
+	let command = request.payload.command;
+
+	exec(`echo '${command}' | cec-client -s`, (error, stdout, stderr) => {
+		if (error != null) {
+			console.error(error);
+			return;
+		}
+		console.log(stdout);
+		console.error(stderr);
+	});
+};
+
 client.openAsync()
 	.then(() => {
 		console.log("iot-hub client opened.");
 		client.onDeviceMethod("sendPS4Command", onSendPS4Command);
+		client.onDeviceMethod("sendCecCommand", onSendCecCommand);
 	})
 	.catch(err => console.error(`could not open iot-hub client. ${err.toString()}`));
